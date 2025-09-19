87485deb08ea309b2de4c461a83636701494922f
Dario Perez Gonzalez <adperezglzz@gmail.com>
2025-09-18 20:00:02 -0600

    Add files via upload

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  semi: true, // usa punto y coma
+  singleQuote: true, // usa comillas simples
+  tabWidth: 2, // tamaño de indentación
+  trailingComma: "es5", // comas finales donde sea válido
+};
